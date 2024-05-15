db347dcdd40342863fbd9c792e80d3967d1570e7
yagords <00001084480050sp@al.educacao.sp.gov.br>
2024-05-15 08:27:39 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() { 
+createCanvas(600,600);
+background("black");
+}
+function draw() {
+stroke("blue");
+fill("red");
+  
+if(mouseIsPressed) {
+  rect(mouseX,mouseY,20,35,);
+  }
+}
